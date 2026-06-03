918c14c00ae129b5af729fd966c4aab3cc06b419
KishoreRaja05 <kishorer1717@gmail.com>
2026-06-03 23:29:35 +0530

    fix api base url to include /api prefix

--- client/src/services/api.jsx
+++ client/src/services/api.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://vaulto-backend-rrap.onrender.com'
+  baseURL: 'https://vaulto-backend-rrap.onrender.com/api',
 })
 
 api.interceptors.request.use((config) => {
